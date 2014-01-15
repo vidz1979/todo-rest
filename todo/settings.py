4a32da88edf00efe5226baf1bb8b95e5289340be
Junior Vidotti <jrvidotti@gmail.com>
2014-01-15 14:05:48 -0300

    directory refactoring

--- todo/settings.py
+++ todo/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'south',
     'rest_framework',
-    'djangular',
-    'todo.core',
+    'core',
 )
 
 MIDDLEWARE_CLASSES = (
--- todo/urls.py
+++ todo/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.conf.urls import patterns, url, include
 from rest_framework.routers import DefaultRouter
-from todo.core import views
+from core import views
 
 admin.autodiscover()
 
@@ -11,7 +11,7 @@ router.register(r'tasks', views.TaskViewSet)
 router.register(r'users', views.UserViewSet)
 
 urlpatterns = patterns('',
-    url(r'^tasks', 'todo.core.views.tasks'),
+    url(r'^tasks', 'core.views.tasks'),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls)),
