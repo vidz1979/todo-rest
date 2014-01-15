d52d8a68e8e589a5d0ced2a17b0240c2c2da5e21
Junior Vidotti <jrvidotti@gmail.com>
2014-01-15 16:13:06 -0300

    static

--- todo/settings.py
+++ todo/settings.py
@@ -85,7 +85,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT = BASE_DIR.child('staticfiles')
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    BASE_DIR.child('static'),
+)
+
 APPEND_SLASH = False
+
+#import os
+#PROJECT_PATH = os.path.dirname(os.path.abspath('/Dev/todo-rest-env2/todo-rest/todo/settings.py'))
+#STATIC_ROOT = 'staticfiles'
+#STATIC_URL = '/static/'
+#os.path.join(PROJECT_PATH, 'static')
